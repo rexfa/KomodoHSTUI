645c75a0e2bc87893aaba28eafc84f7542e00202
rexfa <rex_fa@163.com>
2020-06-29 00:35:05 +0800

    终于文件系统基本ok

--- dist/renderer-process/workspace/workpanel.js
+++ dist/renderer-process/workspace/workpanel.js
@@ -1,16 +1,30 @@
 "use strict";
-//$(function () {
+/* eslint-disable spaced-comment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable max-len */
+// $(function () {
 //    $('#myList a:last-child').tab('show');
 Object.defineProperty(exports, "__esModule", { value: true });
-//});
-let mysql = require('mysql');
-let fs = require('fs');
-let osenv = require('osenv');
+// });
+// import mysql = require('mysql');
+const fs = require("fs");
+const osenv = require("osenv");
 // 引入 aysnc模块
-let async = require('async');
+const async = require("async");
 // 引入path模块
-let path = require('path');
+const path = require("path");
+const drivelist = require("drivelist");
+/**
+ * 文件类
+ */
 class FileClass {
+    /**
+     *
+     * @param {string} fileName 文件名
+     * @param {string | Buffer | URL} path 路径
+     * @param {string} type 类型
+     * @param {number} size 大小
+     */
     constructor(fileName, path, type, size) {
         this.fileName = fileName;
         this.path = path;
@@ -18,43 +32,64 @@ class FileClass {
         this.size = size;
     }
 }
+/*
 function connectionDB() {
-    let connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '123qwe',
-        database: 'komodohstauto'
-    });
-}
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123qwe', // or the original password : 'apaswword'
+    database: 'komodohstauto',
+  });
+}*/
+/**
+ * @return {string} 返回当前用户目录
+ */
 function getUsersHomeFolder() {
+    //console.log('Home is '+osenv.home());
     return osenv.home();
 }
 /**
- * 获取目录第一步
+ * 获取目录第一步,需要制定目录全路径名称
+ * @param {string | Buffer | URL} folderPath 目录
  */
 function getFilesInFolder(folderPath) {
     console.log('Folder is ' + folderPath);
-    fs.readdir(folderPath, function (err, files) {
+    removeAllFileAndDir();
+    const lbCurrentdisk = document.getElementById('hstautoworkpanel-storepanel-currentdisk');
+    lbCurrentdisk.innerHTML = folderPath;
+    fs.readdir(folderPath, (err, files) => {
         if (err) {
             console.log('Folder is not load.' + err.message);
             return;
         }
-        inspectAndDescribeFiles(folderPath, files, displayFiles);
+        //console.log('Folders files length is '+files.length);
+        inspectAndDescribeFiles(folderPath, files);
     });
 }
-function inspectAndDescribeFiles(folderPath, files, displayFiles) {
-    async.map(files, function (file, asyncCB) {
-        let resolveFilePath = path.resolve(folderPath, file);
-        inspectAndDescribeFile(resolveFilePath, asyncCB);
+/**
+ * 异步读取文件和目录
+ * @param {string | Buffer | URL} folderPath 目录
+ * @param {string[]} files 文件数组
+ */
+function inspectAndDescribeFiles(folderPath, files) {
+    async.map(files, function (file) {
+        const resolveFilePath = path.resolve(folderPath, file); // 返回相对当前路径呃绝对路径
+        console.log('Get Path Folders is ' + resolveFilePath);
+        inspectAndDescribeFile(resolveFilePath);
     }, displayFiles);
 }
-function inspectAndDescribeFile(filePath, displayFiles) {
-    let result = new FileClass(path.basename(filePath), filePath, '', 0);
+/**
+ * 检擦文件和目录
+ * @param {string | Buffer | URL} filePath 目录
+ */
+function inspectAndDescribeFile(filePath) {
+    const result = new FileClass(path.basename(filePath), filePath, '', 0);
     fs.stat(filePath, (err, stat) => {
         if (err) {
-            displayFiles(err);
+            console.log(err);
         }
         else {
+            //console.log('stat get isFile '+stat.isFile());
             if (stat.isFile()) { // 判断是否是文件
                 result.type = 'file';
                 result.size = stat.size;
@@ -63,43 +98,126 @@ function inspectAndDescribeFile(filePath, displayFiles) {
                 result.type = 'directory';
                 result.size = stat.size;
             }
-            displayFiles(err, result);
+            displayFile(result);
         }
     });
 }
+// eslint-disable-next-line valid-jsdoc
+/**
+ * 显示
+ * @param {Error} err 错误
+ * @param {Array<FileClass>} files 文件数组
+ */
 function displayFiles(err, files) {
     if (err) {
         return alert('sorry, we could not display your files');
     }
     files.forEach(displayFile);
 }
+/**
+ * 显示文件
+ * @param {FileClass} file 类
+ */
 function displayFile(file) {
-    console.log(file.fileName + ' ' + file.type + ' ' + file.size);
-    let mainArea = document.getElementById('nav-store-items');
-    let template = document.querySelector('#store-item-template');
+    //console.log(file.fileName +' ' + file.type+' '+file.size);
+    const mainArea = document.getElementById('hstautoworkpanel-store-items');
+    const template = document.getElementById('store-item-template');
     // 创建模板实列的副本
-    let clone = document.importNode(template, true);
+    const clone = document.importNode(template, true);
+    clone.classList.remove('is-templete');
     // 加入文件名及对应的图标
     clone.querySelector('img').src = `../assets/img/${file.type}.svg`;
-    clone.querySelector('.filename').innerHTML = function () { if (file.fileName.length > 15)
-        return file.fileName.substr(0, 12) + '...';
-    else
-        return file.fileName; }();
+    clone.querySelector('.filename').innerHTML = function () {
+        if (file.fileName.length > 15)
+            return file.fileName.substr(0, 12) + '...';
+        else
+            return file.fileName;
+    }();
     clone.id = 'storeid-' + file.type + '-' + file.fileName;
     if (file.type == 'directory') {
         clone.addEventListener('click', function () {
-            alert("d " + file.fileName);
+            alert('d ' + file.fileName);
         });
     }
     else {
         clone.addEventListener('click', function () {
-            alert("f " + file.fileName);
+            alert('f ' + file.fileName);
         });
     }
     mainArea.appendChild(clone);
 }
+/**
+ * 删除所有显示
+ */
+function removeAllFileAndDir() {
+    const mainArea = document.getElementById('hstautoworkpanel-store-items');
+    // let template = document.getElementById('store-item-template');
+    // 这种方式略显粗暴
+    mainArea.innerHTML = '';
+    // mainArea.appendChild(template);
+    // console.log(mainArea.childNodes.length);
+    // while(mainArea.childNodes.length>1){
+    //  mainArea.removeChild(mainArea.lastElementChild);
+    // }
+}
+/**
+ * 移除所有显示的盘符
+ */
+function removeAllDisks() {
+    const disksdiv = document.getElementById('hstautoworkpanel-store-disks');
+    disksdiv.innerHTML = '';
+}
+/**
+ * 获取所有驱动器
+ */
+async function getDriversInfo() {
+    removeAllDisks();
+    // var current_disk = __dirname.substr(0,2).toLowerCase();
+    const disksdiv = document.getElementById('hstautoworkpanel-store-disks');
+    const template = document.getElementById('store-disk-template');
+    const drives = await drivelist.list();
+    drives.forEach(function (driver) {
+        // 创建模板实列的副本
+        driver.mountpoints.forEach(function (mountpoint) {
+            const clone = document.importNode(template, true);
+            clone.classList.remove('is-templete');
+            clone.id = 'diskid-' + mountpoint.path;
+            clone.querySelector('.diskname').innerHTML = mountpoint.path;
+            disksdiv.appendChild(clone);
+        });
+    });
+    // let diskinfo = require('diskinfo');
+    /*
+    diskinfo.getDrives(function(err:Error, aDrives:any) {
+      //遍历所有磁盘信息
+      for (var i = 0; i < aDrives.length; i++) {
+          // 创建模板实列的副本
+          let clone = document.importNode(template, true);
+          clone.classList.remove('is-templete');
+          clone.id='diskid-'+aDrives[i].mounted;
+          clone.querySelector('.diskname').innerHTML=aDrives[i].mounted;
+          disksdiv.appendChild(clone);
+        //只获取当前磁盘信息
+        // if( aDrives[i].mounted.toLowerCase() == current_disk ){
+        //   //盘符号
+        //   var mounted = 'mounted ' + aDrives[i].mounted;
+        //   //总量
+        //   var total  = 'total ' + (aDrives[i].blocks /1024 /1024 /1024).toFixed(0) + "gb";
+        //   //已使用
+        //   var used = 'used ' + (aDrives[i].used /1024 /1024 /1024).toFixed(0) + "gb";
+        //   //可用
+        //   var available = 'available ' + (aDrives[i].available /1024 /1024 /1024).toFixed(0) + "gb";
+        //   //使用率
+        //   var capacity = 'capacity ' + aDrives[i].capacity;
+        //   console.log(mounted+"\r\n"+total+"\r\n"+used+"\r\n"+available+"\r\n"+capacity);
+        // }
+      }
+    });*/
+}
 document.getElementById('hstautoworkpanel-storepanel-refresh').addEventListener('click', function (event) {
     getFilesInFolder(getUsersHomeFolder());
+    // getDriversInfo();
 });
 getFilesInFolder(getUsersHomeFolder());
+getDriversInfo();
 //# sourceMappingURL=workpanel.js.map
--- src/renderer-process/workspace/workpanel.ts
+++ src/renderer-process/workspace/workpanel.ts
@@ -1,65 +1,101 @@
-//$(function () {
+/* eslint-disable spaced-comment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable max-len */
+// $(function () {
 //    $('#myList a:last-child').tab('show');
 
-import { Stats } from "fs";
+import {Stats} from 'fs';
 
-//});
-let mysql = require('mysql');
-let fs = require('fs');
-let osenv = require('osenv');
+// });
+// import mysql = require('mysql');
+import fs = require('fs');
+
+import osenv = require('osenv');
 // 引入 aysnc模块
-let async = require('async');
+import async = require('async');
 // 引入path模块
-let path = require('path');
-class FileClass{
+import path = require('path');
+import drivelist = require('drivelist');
+/**
+ * 文件类
+ */
+class FileClass {
   fileName: string;
   path: string | Buffer | URL;
   type: string;
-  size:Number;
-  constructor(fileName: string, path: string | Buffer | URL,type: string,size:Number)
-  {
+  size:number;
+  /**
+   *
+   * @param {string} fileName 文件名
+   * @param {string | Buffer | URL} path 路径
+   * @param {string} type 类型
+   * @param {number} size 大小
+   */
+  constructor(fileName: string, path: string | Buffer | URL, type: string, size:number) {
     this.fileName = fileName;
     this.path = path;
     this.type = type;
     this.size = size;
   }
 }
-function connectionDB(){
-    let connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : '123qwe', // or the original password : 'apaswword'
-        database : 'komodohstauto'
-    });
-}
-function getUsersHomeFolder() {
-    return osenv.home();
-}
+/*
+function connectionDB() {
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123qwe', // or the original password : 'apaswword'
+    database: 'komodohstauto',
+  });
+}*/
 /**
- * 获取目录第一步
+ * @return {string} 返回当前用户目录
+ */
+function getUsersHomeFolder():string {
+  //console.log('Home is '+osenv.home());
+  return osenv.home();
+}
+
+/**
+ * 获取目录第一步,需要制定目录全路径名称
+ * @param {string | Buffer | URL} folderPath 目录
  */
 function getFilesInFolder(folderPath:string | Buffer | URL) {
   console.log('Folder is '+folderPath);
-  fs.readdir(folderPath, function(err:Error,files:Array<File>){
+  removeAllFileAndDir();
+  const lbCurrentdisk = document.getElementById('hstautoworkpanel-storepanel-currentdisk');
+  lbCurrentdisk.innerHTML = folderPath as string;
+  fs.readdir(folderPath, (err, files)=> {
     if (err) {
       console.log('Folder is not load.'+err.message);
       return;
-      }
-      inspectAndDescribeFiles(folderPath, files, displayFiles);
+    }
+    //console.log('Folders files length is '+files.length);
+    inspectAndDescribeFiles(folderPath, files);
   });
 }
-function inspectAndDescribeFiles(folderPath:string | Buffer | URL, files:Array<File>, displayFiles:Function){
-  async.map(files, function(file:File, asyncCB:Function){
-  let resolveFilePath = path.resolve(folderPath, file);
-  inspectAndDescribeFile(resolveFilePath, asyncCB);
+/**
+ * 异步读取文件和目录
+ * @param {string | Buffer | URL} folderPath 目录
+ * @param {string[]} files 文件数组
+ */
+function inspectAndDescribeFiles(folderPath:string | Buffer | URL, files:string[]) {
+  async.map(files, function(file) {
+    const resolveFilePath = path.resolve(folderPath as string, file);// 返回相对当前路径呃绝对路径
+    console.log('Get Path Folders is '+resolveFilePath);
+    inspectAndDescribeFile(resolveFilePath);
   }, displayFiles);
 }
-function inspectAndDescribeFile(filePath:string | Buffer | URL, displayFiles:Function) {
-  let result = new FileClass(path.basename(filePath), filePath,'',0);
+/**
+ * 检擦文件和目录
+ * @param {string | Buffer | URL} filePath 目录
+ */
+function inspectAndDescribeFile(filePath:string | Buffer | URL) {
+  const result = new FileClass(path.basename(filePath as string), filePath, '', 0);
   fs.stat(filePath, (err:Error, stat:Stats) => {
     if (err) {
-      displayFiles(err);
+      console.log(err);
     } else {
+      //console.log('stat get isFile '+stat.isFile());
       if (stat.isFile()) { // 判断是否是文件
         result.type = 'file';
         result.size = stat.size;
@@ -68,41 +104,124 @@ function inspectAndDescribeFile(filePath:string | Buffer | URL, displayFiles:Fun
         result.type = 'directory';
         result.size = stat.size;
       }
-      displayFiles(err, result);
+      displayFile(result);
     }
   });
 }
-function displayFiles(err:Error,files:Array<FileClass>) {
-    if (err) {
-      return alert('sorry, we could not display your files');
-    }
-    files.forEach(displayFile);
-}
-
-function displayFile(file:FileClass) {
-  console.log(file.fileName +' ' + file.type+' '+file.size);
-  let mainArea = document.getElementById('nav-store-items');
-  let template = document.querySelector('#store-item-template');
-  // 创建模板实列的副本
-  let clone = document.importNode(template, true);
-  
-  // 加入文件名及对应的图标
-  clone.querySelector<"img">('img').src = `../assets/img/${file.type}.svg`;
-  clone.querySelector('.filename').innerHTML = function():string{if(file.fileName.length>15) return file.fileName.substr(0,12)+'...'; else return file.fileName;}();
-  clone.id='storeid-'+file.type+'-'+file.fileName;
-  if(file.type=='directory'){
-    clone.addEventListener('click',function(){
-      alert("d "+file.fileName);
-    });
+// eslint-disable-next-line valid-jsdoc
+/**
+ * 显示
+ * @param {Error} err 错误
+ * @param {Array<FileClass>} files 文件数组
+ */
+function displayFiles(err:Error, files:Array<FileClass>) :void{
+  if (err) {
+    return alert('sorry, we could not display your files');
   }
-  else {    
-    clone.addEventListener('click',function(){
-      alert("f "+file.fileName);
-    });    
+  files.forEach(displayFile);
+}
+/**
+ * 显示文件
+ * @param {FileClass} file 类
+ */
+function displayFile(file:FileClass) {
+  //console.log(file.fileName +' ' + file.type+' '+file.size);
+  const mainArea = document.getElementById('hstautoworkpanel-store-items');
+  const template = document.getElementById('store-item-template');
+  // 创建模板实列的副本
+  const clone = document.importNode(template, true);
+  clone.classList.remove('is-templete');
+  // 加入文件名及对应的图标
+  clone.querySelector<'img'>('img').src = `../assets/img/${file.type}.svg`;
+  clone.querySelector('.filename').innerHTML = function():string {
+    if (file.fileName.length>15) return file.fileName.substr(0, 12)+'...'; else return file.fileName;
+  }();
+  clone.id='storeid-'+file.type+'-'+file.fileName;
+  if (file.type=='directory') {
+    clone.addEventListener('click', function() {
+      alert('d '+file.fileName);
+    });
+  } else {
+    clone.addEventListener('click', function() {
+      alert('f '+file.fileName);
+    });
   }
   mainArea.appendChild(clone);
 }
-document.getElementById('hstautoworkpanel-storepanel-refresh').addEventListener('click',function(event:MouseEvent){
+/**
+ * 删除所有显示
+ */
+function removeAllFileAndDir():void {
+  const mainArea = document.getElementById('hstautoworkpanel-store-items');
+  // let template = document.getElementById('store-item-template');
+  // 这种方式略显粗暴
+  mainArea.innerHTML='';
+  // mainArea.appendChild(template);
+  // console.log(mainArea.childNodes.length);
+  // while(mainArea.childNodes.length>1){
+  //  mainArea.removeChild(mainArea.lastElementChild);
+  // }
+}
+/**
+ * 移除所有显示的盘符
+ */
+function removeAllDisks():void {
+  const disksdiv = document.getElementById('hstautoworkpanel-store-disks');
+  disksdiv.innerHTML='';
+}
+/**
+ * 获取所有驱动器
+ */
+async function getDriversInfo() {
+  removeAllDisks();
+  // var current_disk = __dirname.substr(0,2).toLowerCase();
+  const disksdiv = document.getElementById('hstautoworkpanel-store-disks');
+  const template = document.getElementById('store-disk-template');
+
+
+  const drives = await drivelist.list();
+  drives.forEach(function(driver) {
+    // 创建模板实列的副本
+    driver.mountpoints.forEach(function(mountpoint) {
+      const clone = document.importNode(template, true);
+      clone.classList.remove('is-templete');
+      clone.id='diskid-'+mountpoint.path;
+      clone.querySelector('.diskname').innerHTML=mountpoint.path;
+      disksdiv.appendChild(clone);
+    });
+  });
+  // let diskinfo = require('diskinfo');
+  /*
+  diskinfo.getDrives(function(err:Error, aDrives:any) {
+    //遍历所有磁盘信息
+    for (var i = 0; i < aDrives.length; i++) {
+        // 创建模板实列的副本
+        let clone = document.importNode(template, true);
+        clone.classList.remove('is-templete');
+        clone.id='diskid-'+aDrives[i].mounted;
+        clone.querySelector('.diskname').innerHTML=aDrives[i].mounted;
+        disksdiv.appendChild(clone);
+      //只获取当前磁盘信息
+      // if( aDrives[i].mounted.toLowerCase() == current_disk ){
+      //   //盘符号
+      //   var mounted = 'mounted ' + aDrives[i].mounted;
+      //   //总量
+      //   var total  = 'total ' + (aDrives[i].blocks /1024 /1024 /1024).toFixed(0) + "gb";
+      //   //已使用
+      //   var used = 'used ' + (aDrives[i].used /1024 /1024 /1024).toFixed(0) + "gb";
+      //   //可用
+      //   var available = 'available ' + (aDrives[i].available /1024 /1024 /1024).toFixed(0) + "gb";
+      //   //使用率
+      //   var capacity = 'capacity ' + aDrives[i].capacity;
+      //   console.log(mounted+"\r\n"+total+"\r\n"+used+"\r\n"+available+"\r\n"+capacity);
+      // }
+    }
+  });*/
+}
+
+document.getElementById('hstautoworkpanel-storepanel-refresh').addEventListener('click', function(event:MouseEvent) {
   getFilesInFolder(getUsersHomeFolder());
+  // getDriversInfo();
 });
 getFilesInFolder(getUsersHomeFolder());
+getDriversInfo();
